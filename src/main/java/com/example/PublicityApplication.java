ce334dfa3ac72159a89bc7363c1cea1d1e16a7d4
Roosevelt <panta_85.x@hotmail.com>
2017-06-01 00:57:07 -0500

    firstDay

---
+++ src/main/java/com/example/PublicityApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicityApplication.class, args);
+	}
+}
